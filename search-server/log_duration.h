5e7d3de19efe7ebaab1bf7ee2f172740b75cf0c3
Artem Mamonov <57220656+mamonovartval@users.noreply.github.com>
2022-03-07 12:59:52 +0300

    updated

--- search-server/log_duration.h
+++ search-server/log_duration.h
@@ -2,30 +2,20 @@
 
 #include <chrono>
 #include <iostream>
+#include <string>
 
 #define PROFILE_CONCAT_INTERNAL(X, Y) X ## Y
 #define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
 #define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
-#define LOG_DURATION_STREAM(...) LogDuration UNIQUE_VAR_NAME_PROFILE(__VA_ARGS__)
-
-int UNIQUE_VAR_NAME_PROFILE;
+#define LOG_DURATION_STREAM(...) LogDuration int(__VA_ARGS__)
 
 class LogDuration {
 public:
 	using Clock = std::chrono::steady_clock;
 
-	LogDuration(std::string s, std::ostream& os = std::cerr)
-		: name_operation_(s), out_(os) {}
+	LogDuration(const std::string& s, std::ostream& os);
 
-	~LogDuration() {
-		using namespace std::chrono;
-		using namespace std::literals;
-
-		const auto end_time = Clock::now();
-		const auto dur = end_time - start_time_;
-		out_ << name_operation_ << ": "s <<
-			duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
-	}
+	~LogDuration();
 
 private:
 	const Clock::time_point start_time_ = Clock::now();
